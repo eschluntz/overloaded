f973e353d93592f31ecf58888615f8458af3fc5c
erik schluntz <eschluntz@gmail.com>
2020-01-11 16:41:15 -0800

    added test

---
+++ test_senum.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import pytest
+from senum import Senum
+
+
+class State(Senum):
+    begin = 1
+    middle = 2
+    end = 3
+
+
+def test_string_compare():
+    state = State.begin
+    assert state == "begin"
+    assert "begin" == state
+
+
+def test_string_set():
+    state = State.begin
+    state += "end"
+    assert state == State.end
+
+
+def test_bad_compare():
+    state = State.begin
+    with pytest.raises(Exception):
+        state == "ending"
